1a36da60d22ac8ad15aaa047a23e6a263377687e
Daniele Manni <BurnedMarshal@users.noreply.github.com>
2020-11-24 20:35:59 +0100

    Binding API client with production environment (#15)

--- src/helpers/client.ts
+++ src/helpers/client.ts
@@ -1,6 +1,7 @@
 import { createClient } from "../../generated/definitions/client";
 
 export const BackofficeClient = createClient({
+  basePath: "/backoffice",
   baseUrl: process.env.REACT_APP_API_URL || "",
   fetchApi: fetch
 });
