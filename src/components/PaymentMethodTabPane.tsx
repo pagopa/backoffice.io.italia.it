806effa8999410b4f26fd6f05b2dc32058f00524
Vito Falco <vitofalco@MacBook-Pro-di-Vito.local>
2020-11-24 14:11:59 +0100

    TapPane nested in tabpanel for paymethods

---
+++ src/components/PaymentMethodTabPane.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { TabPane } from "reactstrap";
+import { PaymentMethod } from "../generated/definitions/PaymentMethod";
+import { useTranslation } from "react-i18next";
+import { format, parseISO } from "date-fns";
+
+type PaymentMethodTabPaneProps = {
+  index: number;
+  el: PaymentMethod;
+};
+
+function PaymentMethodTabPane(element: PaymentMethodTabPaneProps): JSX.Element {
+  const { t, i18n } = useTranslation();
+  return (
+    <TabPane tabId={element.index}>
+      <div className="container my-3">
+        <div className="row">
+          <div className="col-sm-2 font-weight-bold">{t("State")}</div>
+          <div className="col-sm-10">
+            {element.el.payment_instrument_status}
+          </div>
+          <div className="col-12 py-2"></div>
+          <div className="col-sm-2 font-weight-bold">{t("Insert date")}</div>
+          <div className="col-sm-4">
+            {format(
+              parseISO(element.el.payment_instrument_insert_date),
+              "dd/MM/yyyy HH:mm"
+            )}
+          </div>
+          <div className="col-sm-2 font-weight-bold">{t("Update date")}</div>
+          <div className="col-sm-4">
+            {format(
+              parseISO(element.el.payment_instrument_update_date),
+              "dd/MM/yyyy HH:mm"
+            )}
+          </div>
+          <div className="col-12 py-2"></div>
+          <div className="col-sm-2 font-weight-bold">HPAN</div>
+          <div className="col-sm-10 text-break">
+            {element.el.payment_instrument_hpan}
+          </div>
+        </div>
+      </div>
+    </TabPane>
+  );
+}
+
+export default PaymentMethodTabPane;
--- src/components/Paymethods.tsx
+++ src/components/Paymethods.tsx
@@ -2,6 +2,7 @@ import { number } from "io-ts";
 import { Integer } from "italia-ts-commons/lib/numbers";
 import React, { useState } from "react";
 import { TabContent, TabPane, Nav, NavItem, NavLink } from "reactstrap";
+import PaymentMethodTabPane from "./PaymentMethodTabPane";
 import { format, parseISO } from "date-fns";
 import { useTranslation } from "react-i18next";
 import "./Paymethods.css";
@@ -46,38 +47,7 @@ function Paymethods(props) {
       </Nav>
       <TabContent activeTab={activeTab}>
         {props.paylist.map((el: PaymentMethod, index: number) => (
-          <TabPane tabId={index} key={index}>
-            <div className="container my-3">
-              <div className="row">
-                <div className="col-sm-2 font-weight-bold">{t("State")}</div>
-                <div className="col-sm-10">{el.payment_instrument_status}</div>
-                <div className="col-12 py-2"></div>
-                <div className="col-sm-2 font-weight-bold">
-                  {t("Insert date")}
-                </div>
-                <div className="col-sm-4">
-                  {format(
-                    parseISO(el.payment_instrument_insert_date),
-                    "dd/MM/yyyy HH:mm"
-                  )}
-                </div>
-                <div className="col-sm-2 font-weight-bold">
-                  {t("Update date")}
-                </div>
-                <div className="col-sm-4">
-                  {format(
-                    parseISO(el.payment_instrument_update_date),
-                    "dd/MM/yyyy HH:mm"
-                  )}
-                </div>
-                <div className="col-12 py-2"></div>
-                <div className="col-sm-2 font-weight-bold">HPAN</div>
-                <div className="col-sm-10 text-break">
-                  {el.payment_instrument_hpan}
-                </div>
-              </div>
-            </div>
-          </TabPane>
+          <PaymentMethodTabPane index={index} el={el} key={index} />
         ))}
       </TabContent>
     </>
